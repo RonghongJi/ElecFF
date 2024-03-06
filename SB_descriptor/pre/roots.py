0aecd1174b965aeef45e9f3f3eff1b642ef4fb92
Rona Gee <116425211+RonghongJi@users.noreply.github.com>
2024-03-06 10:58:48 +0800

    Update roots.py

--- SB_descriptor/pre/roots.py
+++ SB_descriptor/pre/roots.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-# Copyright 2019-2021 The NeuralIL contributors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Code used to calculate the roots of the spherical Bessel functions of the
-# first kind. Inspired on the C implementation by Jeremy K. Mason, also under
-# an Apache license and included as part of the reference implementation of
-# the spherical Bessel descriptors (doi: 10.1063/1.5111045 ,
-# https://github.com/harharkh/sb_desc ). However, here we use the scipy root
-# finding routines.
-
 import numpy as np
 import scipy as sp
 import scipy.optimize
@@ -71,27 +49,3 @@ class SphericalBesselRoots:
                 left = self.table[order - 1, n]
                 right = self.table[order - 1, n + 1]
                 self.table[order, n] = sp.optimize.brentq(function, left, right)
-
-
-if __name__ == "__main__":
-    # Time the calculation and check the maximum error in the roots.
-    import timeit
-    import datetime
-    test_n_max = 140
-
-    print(f"Building the table of roots up to n_max={test_n_max}")
-    start = timeit.default_timer()
-    roots = SphericalBesselRoots(test_n_max)
-    end = timeit.default_timer()
-    print("Time elapsed:", str(datetime.timedelta(seconds=end - start)))
-
-    print("Computing the maximum error of the roots")
-    start = timeit.default_timer()
-    max_error = 0.
-    for order in range(0, test_n_max + 1):
-        function = jax.jit(functions.create_j_l(order))
-        for n in range(test_n_max + 2 - order):
-            max_error = max(max_error, function(roots.table[order, n]))
-    end = timeit.default_timer()
-    print("Time elapsed:", str(datetime.timedelta(seconds=end - start)))
-    print("Maximum error:", max_error)
